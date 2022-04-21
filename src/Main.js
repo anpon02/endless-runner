972f35c7dd239670e00a79445ee5ff98ab69c324
“Purisen” <gmarcelsmith@gmail.com>
2022-04-21 09:04:34 -0700

    initial physics code in main

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,15 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 800},
+            debug: false
+        }
+    },
+
     scene: [ Menu, Play]
 }
 
